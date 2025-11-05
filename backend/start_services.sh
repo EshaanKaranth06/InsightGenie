4404623f34140a9862debfb8f168cb5118cce8b2
EshaanKaranth06 <eshaankaranth06@gmail.com>
2025-11-06 00:10:08 +0530

    yup

--- backend/start_services.sh
+++ backend/start_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+export PYTHONPATH=.
 echo "Starting Celery Worker..."
 # This command now works because Render is in the root and can find 'backend'
 celery -A backend.celery_app worker --loglevel=INFO -P gevent -c 10 &
